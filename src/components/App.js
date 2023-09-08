8641b185617675b3d30ec8bd7e2e38492a907e4c
Maxim Grassi <grassimacsim@yandex.ru>
2023-09-08 18:06:29 +0300

    feat: выполнен лендинг

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Main from './Main.js';
+
+function App() {
+  return (
+    <div className="page">
+      <Main></Main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,38 @@
+import logo from '../images/logo.svg';
+import mainpic from '../images/mainpic.png';
+import tgicon from '../images/tg-icon.svg';
+import vkicon from '../images/vk-icon.svg';
+
+function Main() {
+  return (
+    <div className="main">
+      <div class="main__info">
+        <img src={logo} className="logo"></img>
+        <h1 className="main__title">ТВОЯ ТЕРРИТОРИЯ СИЛЫ</h1>
+        <p className="main__startdate">СТАРТ ПРОДАЖ 9 СЕНТЯБРЯ 2023 ГОДА</p>
+        <p className="main__ordertext">Заказать экскурсию по комфортной жизни прямо сейчас:</p>
+        <button className="main__button">+7 499 229-75-75</button>
+      </div>
+      <img src={mainpic} className="main__pic" alt="Фотография дома"></img>
+      <footer className="footer">
+        <p className="footer__address">
+          Московская область, Можайский район Координаты: 55.587218, 36.083127
+        </p>
+        <ul className="buttons">
+          <li>
+            <a className="button__link">
+              <img src={tgicon} />
+            </a>
+          </li>
+          <li>
+            <a className="button__link">
+              <img src={vkicon} />
+            </a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
+}
+
+export default Main;
